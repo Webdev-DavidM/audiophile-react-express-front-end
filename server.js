b745a9f3f148b7ae700776350b0d6b1ee2cf15e2
David <>
2022-03-16 11:37:39 +0000

    port added

--- server.js
+++ server.js
@@ -8,4 +8,8 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000);
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log('listening on port', port);
+});
